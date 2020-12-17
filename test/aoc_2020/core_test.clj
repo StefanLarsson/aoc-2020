f4a94d720fa115af88dacbe0e1acae2e33f92acb
Stefan Larsson <p.stefan.larsson@gmail.com>
2020-12-17 20:36:50 +0100

    Add tests
    Remove dummy failing test

--- test/aoc_2020/core_test.clj
+++ test/aoc_2020/core_test.clj
@@ -2,6 +2,12 @@
   (:require [clojure.test :refer :all]
             [aoc-2020.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest day1-test-part1
+  (testing "That we can find two numbers in a set of numbers that sum to 2020")
+    (is (= #{1721 299} (set (as-sum-of-n 2 2020 '(1721 979 366 299 675 1456))))))
+
+(deftest day1-test-part2
+  (testing "That we can find three numbers in a set of numbers that sum to 2020")
+    (is (= #{979 366 675} (set (as-sum-of-n 3 2020 '(1721 979 366 299 675 1456))))))
+
+
